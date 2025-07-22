0e5663f400c74a57eaa62cf45056fba6e1f4dee4
T772 <arminnessie@gmail.com>
2025-07-22 16:31:51 +0200

    Fix the rest of clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,81 @@
-use error_chain::error_chain;
 use std::env;
 use std::fs::File;
-use std::io::Read;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::num::Wrapping;
 use std::path::Path;
 use std::process;
 
-extern crate reqwest;
-
-error_chain! {
-     foreign_links {
-         Io(std::io::Error);
-         HttpRequest(reqwest::Error);
-     }
-}
-
 #[tokio::main]
-async fn download_file(target: String) -> Result<()> {
+async fn download_file(target: String) -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get(&target).await?;
     let path = Path::new("./tempfile_hyperlink-one-time-pad");
-    let mut file = match File::create(path) {
-        Err(why) => panic!("couldn't create {why}"),
-        Ok(file) => file,
-    };
+    let mut file = File::create(path)?;
     let content = response.bytes().await?;
     file.write_all(&content)?;
     Ok(())
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let param_amount = args.len();
-    if param_amount < 4 {
-        println!("\nExample:\nhyperlink-one-time-pad \"secret_stuff.zip\" add http://example.com/vid.mp4 http://example.com/data.rar\n\nDescription:\nThe above example uses two files from the internet (both should have a bigger file size than the file to encrypt) that are both downloaded automatically and \"layed over\" the file to encrypt. Decrypting works the same way using the parameter sub instead of add. You only need to memorize the files that are online available and don't need to store or exchange huge keys, which is a negative point with the normal one-time-pad. It is also possible to use local files as keys instead of hyperlinks.\n\nNotes:\n- Remember that the internet providers may safe the files you download. Use this on top of normal encryption methods.\n- The order of the key parameters does not matter.\n- Hyperlinks must start with http:// or https://.");
+    if args.len() < 4 {
+        println!(
+            "Example:\nhyperlink-one-time-pad \"secret_stuff.zip\" add http://example.com/vid.mp4 http://example.com/data.rar\n\nDescription:\nThe above example uses two files from the internet (both should have a bigger file size than the file to encrypt) that are both downloaded automatically and \"layed over\" the file to encrypt. Decrypting works the same way using the parameter sub instead of add. You only need to memorize the files that are online available and don't need to store or exchange huge keys, which is a negative point with the normal one-time-pad. It is also possible to use local files as keys instead of hyperlinks.\n\nNotes:\n- Remember that the internet providers may safe the files you download. Use this on top of normal encryption methods.\n- The order of the key parameters does not matter.\n- Hyperlinks must start with http:// or https://."
+        );
         process::exit(0);
     }
 
     if !Path::new(&args[1]).exists() {
-        println!("Error: File {} does not exist.", args[1]);
-        process::exit(0);
+        eprintln!("Error: File {} does not exist.", args[1]);
+        process::exit(1);
     }
 
-    let mut encrypt = false;
-    if args[2] == "add" {
-        encrypt = true;
-    } else if args[2] != "sub" {
-        println!("Error: Either use add for encrypting or sub for decrypting.");
-        process::exit(0);
-    }
+    let encrypt = match args[2].as_str() {
+        "add" => true,
+        "sub" => false,
+        _ => {
+            eprintln!("Error: Use 'add' to encrypt or 'sub' to decrypt.");
+            process::exit(1);
+        }
+    };
 
-    let mut file = File::open(&args[1]).unwrap();
-    let filesize = file.metadata().unwrap().len();
-    let length: usize = filesize as usize;
-    let mut vec = vec![0u8; length];
-    let _count = file.read(vec.as_mut_slice()).unwrap();
+    let mut file = File::open(&args[1])?;
+    let filesize = file.metadata()?.len();
+    let mut data = vec![0u8; filesize as usize];
+    file.read_exact(&mut data)?;
 
-    for x in 3..param_amount {
-        let mut online = false;
-        if args[x].starts_with("http://") ^ args[x].starts_with("https://") {
-            print!("({}) Online key ", x - 2);
-            let _a = download_file(args[x].clone());
-            online = true;
+    for (i, arg) in args.iter().skip(3).enumerate() {
+        let is_online = arg.starts_with("http://") || arg.starts_with("https://");
+        print!(
+            "({}) {} key ",
+            i + 1,
+            if is_online { "Online" } else { "Local" }
+        );
+
+        let mut key_data = Vec::new();
+        if is_online {
+            download_file(arg.clone())?;
+            File::open("tempfile_hyperlink-one-time-pad")?.read_to_end(&mut key_data)?;
         } else {
-            print!("({}) Local key ", x - 2);
+            File::open(arg)?.read_to_end(&mut key_data)?;
         }
 
-        let mut file2;
-        if online {
-            file2 = File::open(&"tempfile_hyperlink-one-time-pad").unwrap();
-        } else {
-            file2 = File::open(&args[x]).unwrap();
-        }
-        let filesize2 = file2.metadata().unwrap().len();
-        let length2: usize = filesize2 as usize;
-        let mut vec2 = vec![0u8; length2];
-        let count2 = file2.read(vec2.as_mut_slice()).unwrap();
-        let times: f32 = (length2 as f32) / (length as f32);
-        println!("with {count2} bytes -> {times} times layed over.");
+        let times = key_data.len() as f32 / data.len() as f32;
+        println!(
+            "with {} bytes -> {:.2} times layed over.",
+            key_data.len(),
+            times
+        );
 
-        let mut j = 0;
-        for i in 0..length2 {
-            let foo: Wrapping<u8>;
-            if encrypt {
-                foo = Wrapping(vec[j]) + Wrapping(vec2[i]);
+        for (i, &key_byte) in key_data.iter().enumerate() {
+            let data_idx = i % data.len();
+            data[data_idx] = if encrypt {
+                (Wrapping(data[data_idx]) + Wrapping(key_byte)).0
             } else {
-                foo = Wrapping(vec[j]) - Wrapping(vec2[i]);
-            }
-            vec[j] = foo.0;
-            j += 1;
-            if j > length - 1 {
-                j = 0;
-            }
+                (Wrapping(data[data_idx]) - Wrapping(key_byte)).0
+            };
         }
     }
-    let mut file = File::create(&args[1])?;
-    file.write_all(&vec)?;
+
+    File::create(&args[1])?.write_all(&data)?;
     Ok(())
 }
